8f607a7ca5951522e21cc288c32620bef011d6e8
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-15 07:35:37 +0530

    demo

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = GetElementById("name");
+
+let profession = GetElementById("profession");
+
+if(name.length || profession.length < 3){
+    alert("name should atleast threee character");
+}
+
+let age = GetElementById("age");
